79cc4621eb94b7ea7330cf8badf1f7af542d9bc8
nauvalucu21 <98734156+nauvalucu21@users.noreply.github.com>
2022-09-24 23:59:18 +0700

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+//import mongoose
+import mongoose from "mongoose";
+// import routes
+import route from "./routes/index.js";
+//import cors
+import cors from "cors";
+
+import bodyParser from "body-parser";
+// construct express function
+const app = express();
+ 
+// connect ke database mongoDB
+mongoose.connect("mongodb://localhost:27017/nodejs_db",{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', (error)=> console.error(error));
+db.once('open', () => console.log('Database Connected'));
+ 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+app.use('/product',route);
+ 
+// listening to port
+app.listen('3000',()=> console.log('Server Running at port: 3000'));
